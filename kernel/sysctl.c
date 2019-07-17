2e86c6f345e357eb39806408886710af5d9c2abf
Andy Lavr <andy.lavr@gmail.com>
2019-07-17 07:54:47 +0300

    kernel: timer: Make hrtimer granularity and minimum hrtimeout...
    ...configurable in sysctl.
    
    Signed-off-by: Andy Lavr <andy.lavr@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -141,7 +141,9 @@ static int one_thousand = 1000;
 #ifdef CONFIG_SCHED_BMQ
 extern int sched_yield_type;
 #endif
-#ifdef CONFIG_PRINTK
+extern int hrtimer_granularity_us;
+extern int hrtimeout_min_us;
+#if defined(CONFIG_PRINTK) || defined(CONFIG_SCHED_MUQSS)
 static int ten_thousand = 10000;
 #endif
 #ifdef CONFIG_PERF_EVENTS
@@ -1100,6 +1102,24 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &two,
 	},
 #endif
+	{
+		.procname	= "hrtimer_granularity_us",
+		.data		= &hrtimer_granularity_us,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec_minmax,
+		.extra1		= &one,
+		.extra2		= &ten_thousand,
+	},
+	{
+		.procname	= "hrtimeout_min_us",
+		.data		= &hrtimeout_min_us,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= &proc_dointvec_minmax,
+		.extra1		= &one,
+		.extra2		= &ten_thousand,
+	},
 #if defined(CONFIG_S390) && defined(CONFIG_SMP)
 	{
 		.procname	= "spin_retry",
